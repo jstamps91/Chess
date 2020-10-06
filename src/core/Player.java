5d63793823c4f438f997950b556e625a9fcc2316
Joshua Stamps <stampsmed91@gmail.com>
2020-10-06 15:12:43 -0500

    Update Exceptions for Player class

--- src/core/Player.java
+++ src/core/Player.java
@@ -17,42 +17,89 @@ import javax.swing.*;
 
 public class Player implements Serializable {
 
+    private String name;
+    private Integer gamesPlayed;
+    private Integer gamesWon;
 
     public Player(String name) {
 
+        this.name = name.trim();
+        gamesPlayed = new Integer(0);
+        gamesWon = new Integer(0);
     }
 
     public String name() {
 
+        return name;
     }
 
     public Integer gamesPlayed() {
 
+        return gamesPlayed;
     }
 
     public Integer gamesWon() {
 
+        return gamesWon;
     }
 
     public Integer winPercentage() {
 
+        return new Integer(gamesWon * 100) / gamesPlayed;
     }
 
     public void updateGamesPlayed() {
 
+        gamesPlayed++;
     }
 
     public void updateGamesWon() {
 
+        gamesWon++;
     }
 
     public static ArrayList<Player> fetch_players() {
 
+        Player tempPlayer;
+        ObjectInputStream input = null;
+        ArrayList<Player> players = new ArrayList<Player>();
+
+        try {
+            File in = new File(System.getProperty("user.dir") + File.separator + "chessgamedata.txt");
+            input = new ObjectInputStream(new FileInputStream(in));
+
+            try {
+                while (true) {
+                    tempPlayer = (Player) input.readObject();
+                    players.add(tempPlayer);
+                }
+            } catch (EOFException e) {
+                input.close();
+            }
+
+        } catch (FileNotFoundException e) {
+            players.clear();
+            return players;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            try {
+                input.close();
+            } catch (IOException ioe) {}
+            JOptionPane.showMessageDialog(null, "Unable to read required game files!");
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Game file corrupt! Click OK to continue building new file!");
+
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        return players;
     }
 
     public void update_player() {
 
     }
-
 
 }
