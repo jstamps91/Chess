ded0b07447e11efa74586b47cde98ba0641dc7cd
Joshua Stamps <stampsmed91@gmail.com>
2020-10-10 09:59:54 -0500

    Add logic to Pawn.java

--- src/gamepiece/Pawn.java
+++ src/gamepiece/Pawn.java
@@ -13,9 +13,59 @@ public class Pawn extends Piece {
         setColor(color);
     }
 
-
     @Override
-    public ArrayList<Cell> move(Cell[][] pos, int x, int y) {
-        return null;
+    public ArrayList<Cell> move(Cell[][] state, int x, int y) {
+
+        possibleMoves.clear();
+
+        if(getColor() == 0) {
+
+            if(x == 0) {
+                return possibleMoves;
+            }
+
+            if(state[x - 1][y].getPiece() == null) {
+                possibleMoves.add(state[x - 1][y]);
+
+                if(state[4][y].getPiece() == null) {
+                    possibleMoves.add(state[4][y]);
+                }
+            }
+
+            if((y > 0) && (state[x - 1][y - 1].getPiece() != null) && (state[x - 1][y - 1].getPiece().getColor() != this.getColor())) {
+                possibleMoves.add(state[x - 1][y - 1]);
+            }
+
+            if((y < 7) && (state[x - 1][y + 1].getPiece() != null) && (state[x - 1][y + 1].getPiece().getColor() != this.getColor())) {
+                possibleMoves.add(state[x - 1][y + 1]);
+            }
+
+        } else {
+
+            if(x == 8) {
+                return possibleMoves;
+            }
+        }
+
+        if(state[x + 1][y].getPiece() == null) {
+            possibleMoves.add(state[x + 1][y]);
+
+            if(x == 1) {
+
+                if(state[3][y].getPiece() == null) {
+                    possibleMoves.add(state[3][y]);
+                }
+            }
+        }
+
+        if((y > 0) && (state[x + 1][y - 1].getPiece() != null) && (state[x + 1][y - 1].getPiece().getColor() != this.getColor())) {
+            possibleMoves.add(state[x + 1][y - 1]);
+        }
+
+        if((y < 7) && (state[x + 1][y + 1].getPiece() != null) && (state[x + 1][y + 1].getPiece().getColor() != this.getColor())) {
+            possibleMoves.add(state[x + 1][y + 1]);
+        }
+
+        return possibleMoves;
     }
 }
