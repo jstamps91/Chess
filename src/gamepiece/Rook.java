86a37debc887adabe774d2469f145a027d64c0f8
Joshua Stamps <stampsmed91@gmail.com>
2020-10-10 09:47:10 -0500

    Add logic to Rook.java

--- src/gamepiece/Rook.java
+++ src/gamepiece/Rook.java
@@ -13,10 +13,64 @@ public class Rook extends Piece {
         setColor(color);
     }
 
-
     @Override
-    public ArrayList<Cell> move(Cell[][] pos, int x, int y) {
-        return null;
+    public ArrayList<Cell> move(Cell[][] state, int x, int y) {
+
+        possibleMoves.clear();
+
+        int tempX=x-1;
+
+        while(tempX >= 0) {
+            if(state[tempX][y].getPiece()==null) {
+                possibleMoves.add(state[tempX][y]);
+            } else if(state[tempX][y].getPiece().getColor()==this.getColor()) {
+                break;
+            } else {
+                possibleMoves.add(state[tempX][y]);
+                break;
+            }
+            tempX--;
+        }
+        tempX = x + 1;
+
+        while(tempX < 8) {
+            if(state[tempX][y].getPiece()==null) {
+                possibleMoves.add(state[tempX][y]);
+            } else if(state[tempX][y].getPiece().getColor()==this.getColor()) {
+                break;
+            } else {
+                possibleMoves.add(state[tempX][y]);
+                break;
+            }
+            tempX++;
+        }
+        int tempY = y - 1;
+
+        while(tempY >= 0) {
+            if(state[x][tempY].getPiece()==null) {
+                possibleMoves.add(state[x][tempY]);
+            } else if(state[x][tempY].getPiece().getColor()==this.getColor()) {
+                break;
+            } else {
+                possibleMoves.add(state[x][tempY]);
+                break;
+            }
+            tempY--;
+        }
+        tempY = y + 1;
+
+        while(tempY < 8) {
+            if(state[x][tempY].getPiece()==null) {
+                possibleMoves.add(state[x][tempY]);
+            } else if(state[x][tempY].getPiece().getColor()==this.getColor()) {
+                break;
+            } else {
+                possibleMoves.add(state[x][tempY]);
+                break;
+            }
+            tempY++;
+        }
+        return possibleMoves;
     }
 }
 
