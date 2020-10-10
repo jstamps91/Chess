b83a010bf1aab65085e8af803d70cd818667776e
Joshua Stamps <stampsmed91@gmail.com>
2020-10-10 09:40:37 -0500

    Add logic to Bishop.java

--- src/gamepiece/Bishop.java
+++ src/gamepiece/Bishop.java
@@ -15,7 +15,70 @@ public class Bishop extends Piece {
 
 
     @Override
-    public ArrayList<Cell> move(Cell[][] pos, int x, int y) {
-        return null;
+    public ArrayList<Cell> move(Cell[][] state, int x, int y) {
+
+        possibleMoves.clear();
+
+        int tempX = x + 1;
+        int tempY = y - 1;
+
+        while(tempX < 8 && tempY >= 0) {
+            if(state[tempX][tempY].getPiece() == null) {
+                possibleMoves.add(state[tempX][tempY]);
+            } else if(state[tempX][tempY].getPiece().getColor() == this.getColor()) {
+                break;
+            } else {
+                possibleMoves.add(state[tempX][tempY]);
+                break;
+            }
+            tempX++;
+            tempY--;
+        }
+        tempX = x - 1;
+        tempY = y + 1;
+
+        while(tempX >= 0 && tempY < 8) {
+            if(state[tempX][tempY].getPiece() == null) {
+                possibleMoves.add(state[tempX][tempY]);
+            } else if(state[tempX][tempY].getPiece().getColor() == this.getColor()) {
+                break;
+            } else {
+                possibleMoves.add(state[tempX][tempY]);
+                break;
+            }
+            tempX--;
+            tempY++;
+        }
+        tempX = x - 1;
+        tempY = y - 1;
+
+        while(tempX >= 0 && tempY >= 0) {
+            if(state[tempX][tempY].getPiece() == null) {
+                possibleMoves.add(state[tempX][tempY]);
+            } else if(state[tempX][tempY].getPiece().getColor() == this.getColor()) {
+                break;
+            } else {
+                possibleMoves.add(state[tempX][tempY]);
+                break;
+            }
+            tempX--;
+            tempY--;
+        }
+        tempX = x + 1;
+        tempY = y + 1;
+
+        while(tempX < 8 && tempY < 8) {
+            if(state[tempX][tempY].getPiece() == null) {
+                possibleMoves.add(state[tempX][tempY]);
+            } else if(state[tempX][tempY].getPiece().getColor() == this.getColor()) {
+                break;
+            } else {
+                possibleMoves.add(state[tempX][tempY]);
+                break;
+            }
+            tempX++;
+            tempY++;
+        }
+        return possibleMoves;
     }
 }
