7fcc2de67b8ae277169f083a629d881cf2e69f88
Joshua Stamps <stampsmed91@gmail.com>
2020-10-10 09:34:55 -0500

    Add logic to Knight.java

--- src/gamepiece/Knight.java
+++ src/gamepiece/Knight.java
@@ -15,7 +15,20 @@ public class Knight extends Piece {
 
 
     @Override
-    public ArrayList<Cell> move(Cell[][] pos, int x, int y) {
-        return null;
+    public ArrayList<Cell> move(Cell[][] state, int x, int y) {
+
+        possibleMoves.clear();
+
+        int posX[] = {x + 1, x + 1, x + 2, x + 2, x - 1, x - 1, x - 2, x - 2};
+        int posY[] = {y - 2, y + 2, y - 1, y + 1, y - 2, y + 2, y - 1, y + 1};
+
+        for(int i = 0; i < 8; i++) {
+            if(posX[i] >= 0 && posX[i] < 8 && posY[i] >= 0 && posY[i] < 8) {
+                if(state[posX[i]][posY[i]].getPiece() == null || state[posX[i]][posY[i]].getPiece().getColor() != this.getColor()) {
+                    possibleMoves.add(state[posX[i]][posY[i]]);
+                }
+            }
+        }
+        return possibleMoves;
     }
 }
